02a230a37c866dfb725929e675367b68ec3c6129
aqsaaa <aqsa.arshad@coeus-solutions.de>
2015-08-06 17:09:16 +0500

    Done with Result hard data

--- PhP Assignment/Code/Result.php
+++ PhP Assignment/Code/Result.php
@@ -1,49 +1,71 @@
 <?php
 class Result{
 
-    $rollNum;
-    $Name;
-    $fatherName;
-    $TotalMarks;
+   public $rollNum;
+   public $Name;
+   public $fatherName;
+   public $TotalMarks;
+   public $subjectmarks=array();
+   public $marks=array();
+
+    
 }
 
 class StdResult{
-public $users=array(array());
+public $result=array();
  #internee with userType=11
 public function __construct()
 {
- $users[0][0]=1;
- $users[0][1]="aqsa";
- $users[0][2]=11;
- $users[0][3]="aqsa123";
+     $temp=new Result();
 
- $users[1][0]=2;
- $users[1][1]="Sidra";
- $users[1][2]=11;
- $users[1][3]="sidra123";
+     $temp->Name="sidra";
+     $temp->rollNum=1; 
+     $temp->fatherName="waqar";
+     $temp->TotalMarks=901;
+     $temp->marks[]=90;
+     $temp->marks[]=96;
+     $temp->marks[]=70;
+     $temp->marks[]=87;
+     $temp->marks[]=79;
 
+     $result[0]=$temp;
+     
+     $temp->Name="ramsha";
+     $temp->rollNum=2; 
+     $temp->fatherName="jahangir";
+     $temp->TotalMarks=892;
+     $temp->marks[]=76;
+     $temp->marks[]=56;
+     $temp->marks[]=80;
+     $temp->marks[]=87;
+     $temp->marks[]=95;
+     $result[1]=$temp;
 
-#Db admin with userType 31
- $users[2][0]=3;
- $users[2][1]="Muhammad Ali";
- $users[2][2]=31;
- $users[2][3]="ali123";
-
-
- #dataEntry Operator wityh Id 21
-
- $users[3][0]=4;
- $users[3][1]="rabia";
- $users[3][2]=21;
- $users[3][3]="rabia123";
-
-
- $users[4][0]=5;
- $users[4][1]="waqas Javaid";
- $users[4][2]=21;
- $users[4][3]="waqas123";
+     $temp->Name="mustaeen";
+     $temp->rollNum=3; 
+     $temp->fatherName="basit zaki";
+     $temp->TotalMarks=802;
+      $temp->marks[]=87;
+     $temp->marks[]=76;
+     $temp->marks[]=77;
+     $temp->marks[]=67;
+     $temp->marks[]=89;
+     $result[2]=$temp;
+ 
+    $temp->Name="ayesha";
+     $temp->rollNum=4; 
+     $temp->fatherName="akhtar";
+     $temp->TotalMarks=782;
+      $temp->marks[]=64;
+     $temp->marks[]=76;
+     $temp->marks[]=87;
+     $temp->marks[]=57;
+     $temp->marks[]=79;
+    $result[3]=$temp;
 
 }
+
+
 }
 
 
